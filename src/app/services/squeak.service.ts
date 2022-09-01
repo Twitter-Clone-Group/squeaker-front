12f4a3ff9a21717af6bb200e8e437ad57994c4b1
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-09-01 09:20:20 -0400

    squeak service frontend to backend connection

---
+++ src/app/services/squeak.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from "rxjs";
+import {Squeak} from "../models/squeak";
+import {SqueakDTO} from "../models/squeakDTO";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SqueakerService {
+  private squeakUrl: string;
+  private headers: HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    this.squeakUrl = 'http://localhost:8080/squeak';
+    this.headers = new HttpHeaders({'Content-Type' : 'application/json'});
+  }
+
+  public findAll(): Observable<Squeak[]> {
+    return this.http.get<Squeak[]>(this.squeakUrl + `/all`);
+  }
+
+  public save(squeakDTO: SqueakDTO): Observable<Squeak> {
+    return this.http.post<Squeak>(this.squeakUrl, squeakDTO);
+  }
+}
