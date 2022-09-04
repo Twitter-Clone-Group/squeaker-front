aaba78e5ca1691b0b4deaacaf4325aa3a0e0e995
Git-Yee Chu <chugityee2@yahoo.com>
2022-09-04 00:11:15 -0400

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {SqueakComponent} from "./squeak/squeak.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'squeak', component: SqueakComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
