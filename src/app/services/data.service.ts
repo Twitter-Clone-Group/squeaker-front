2e8782dee8e1835d4ab6d56aa8811318d0946828
Git-Yee Chu <chugityee2@yahoo.com>
2022-09-04 02:36:56 -0400

    set squeaker id in data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  private squeakerId = new BehaviorSubject<number>(1);
+  currentSqueakerId = this.squeakerId.asObservable();
+
   constructor() { }
+
+  changeCurrentSqueakerId(id: number) {
+    this.squeakerId.next(id);
+  }
 }
