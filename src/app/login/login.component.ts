f0b9d6378bd938b4b00970759b0c74cd7a3eb538
todd-bowers <toddbows@gmail.com>
2022-09-03 22:12:08 -0400

    User registration works

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Squeaker} from "../models/squeaker";
+import {SqueakerService} from "../services/squeaker.service";
+import {NgForm} from "@angular/forms";
+import {SqueakerDTO} from "../models/squeakerDTO";
 
 @Component({
   selector: 'app-login',
@@ -7,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private squeakerService: SqueakerService) {
+  }
 
   ngOnInit(): void {
   }
+
+  saveSqueaker(sendForm: NgForm): void {
+    const squeakerDTO = new SqueakerDTO(sendForm.value.username, sendForm.value.password)
+    this.squeakerService.save(squeakerDTO).subscribe();
+    sendForm.control.reset()
+  }
+
+  //Need to be able to log in a user and keep track of them
+  //Logged in user should be tied to any tweets created and the profile displayed when that button is clicked
 
   login() {
     return true;
