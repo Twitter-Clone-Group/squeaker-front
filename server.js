a943433f6bf1efd29ba154ac2e732c643986c7b9
Git-Yee Chu <chugityee2@yahoo.com>
2022-09-05 18:37:39 -0400

    correct path name

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname + '/dist/squeaker-zcw'));
+app.use(express.static(__dirname + '/dist/squeaker-ui'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/squeaker-zcw/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/squeaker-ui/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
